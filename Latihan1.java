096950945b190e35caa7e301facf5438b07787bc
mezutizzul <127457577+mezutizzul@users.noreply.github.com>
2023-04-07 09:38:26 +0700

    Add files via upload

---
+++ Latihan1.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum3;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author Ihzani Izzul Haq
+ */
+
+public class Latihan1 {
+    public static void main(String[] args) {
+        Stack st = new Stack();//membuat stack baru
+        st.push("Aku");//push item
+        st.push("Anak");
+        st.push("Indonesia");
+
+        System.out.println("Next : " +st.peek());//peek : Menghasilkan elemen pada top stack, tetapi tidak me remove.
+        st.push("Raya");
+        System.out.println(st.pop());//pop : Menghasilkan elemen pada stack, dan mengambil/menghapus (remove) elemen tersebut
+        st.push("!");
+
+        int count = st.search("Aku");
+        while (count != -1 && count > 1){
+            st.pop();
+            count--;
+        }
+        System.out.println(st.pop());
+        System.out.println(st.empty());//Menghasilkan nilai True jika stack kosong, dan nilai False jika stack berisi elemen.
+    }
+}
